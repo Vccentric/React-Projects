841cb4aea9731c2ed3f80e5b543df737d1a30e97
Vccentric <>
2018-07-23 18:42:08 -0700

    1. Added input field validations. 2. Added placeholders in the input fields.

--- project-2/signup.js
+++ project-2/signup.js
@@ -35,11 +35,43 @@ class SignupForm extends React.Component {
     // function to handle form submit
     handleFormSubmit(e) {
         e.preventDefault();
-        let msg = "These were the values submitted into the Signup Form:\n";
-        for (let field in this.state) {
-            msg += "- " + field + ': ' + this.state[field] + '\n';
+
+        // validate input fields
+        let results = this.validate();
+        let msg = '';
+        if (results && results.pass) { // pass
+            msg = "These were the values submitted into the Signup Form:\n";
+            for (let field in this.state) {
+                msg += "- " + field + ': ' + this.state[field] + '\n';
+            }
+        } else { // fail
+            msg = "Error:\n";
+            for (let i = 0; i < results.errorMessages.length; i++) {
+                msg += results.errorMessages[i];
+            }
         }
+
+        // display message
         alert(msg);
+    }
+
+    // function to handle input validations
+    validate(data) {
+        let results = { // default values
+            'pass': true,
+            'errorMessages': []
+        };
+
+        // check if input field is empty
+        for (let field in this.state) {
+            let value = this.state[field];
+            if (value === '') {
+                let error = "- the field: " + field + ', is required.\n';
+                results.errorMessages.push(error);
+                results.pass = false;
+            }
+        }
+        return results;
     }
 
     render() {
@@ -98,7 +130,15 @@ class InputField extends React.Component {
         return (
             <div>
                 <label htmlFor={this.props.name}>{this.props.label}: </label>
-                <input type={this.props.type} id={this.props.name} name={this.props.name} value={this.props.value} onChange={this.handleInputChange} />
+                <br />
+                <input
+                    type={this.props.type}
+                    id={this.props.name}
+                    name={this.props.name}
+                    value={this.props.value}
+                    onChange={this.handleInputChange}
+                    placeholder={this.props.label}
+                />
                 <br /><br />
             </div>
         );
