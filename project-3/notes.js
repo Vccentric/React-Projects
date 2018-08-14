90bf2795d820df56dc7c9f52e7ad84fc9923a366
Vccentric <>
2018-08-13 17:41:04 -0700

    Added basic input field text area component.

--- project-3/notes.js
+++ project-3/notes.js
@@ -8,8 +8,48 @@
  * @version     1.0.0
  */
 
+// input field component
+class InputField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.clear = this.clear.bind(this);
+        this.create = this.create.bind(this);
+        this.state = {
+            value: ''
+        };
+    }
+
+    // function to handle changes in the textarea
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    // function to clear/empty the textarea
+    clear() {
+        this.setState({ value: '' });
+    }
+
+    // function to create a new note from the data in the textarea
+    create() {
+        // todo
+    }
+
+    render() {
+        return (
+            <fieldset>
+                <textarea value={this.state.value} onChange={this.handleChange} placeholder="Enter Text"></textarea>
+                <div>
+                    <button onClick={this.clear}>Clear</button>
+                    <button onclick={this.create}>Enter</button>
+                </div>
+            </fieldset>
+        );
+    }
+}
+
 // initialize and render on the page
 ReactDOM.render(
-    <h1>Hello World!</h1>,
+    <InputField />,
     document.getElementById('root')
 );
